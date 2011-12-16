b5796384319c8cf0572150b341e89997f40cf8d6
Shuhao <admin@thekks.net>
2011-12-16 16:35:54 -0500

    Some more updates...
    Some more useless (featureless) updates for pypi.. took a while..

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
   author = "Shuhao Wu",
   author_email = "shuhao@shuhaowu.com",
   description = ("An object mapper for Riak similar to mongokit, couchdbkit and"
-                 " riakalchemy"),
+                 " riakalchemy for Riak"),
   license = "GPLv3",
   keywords = "riak object mapper riakkit database",
   url = "https://github.com/ultimatebuster/riakkit",
   packages = ["riakkit"],
-  classifier = [
+  classifiers = [
     "Development Status :: 3 - Alpha",
     "License :: OSI Approved :: GNU General Public License (GPL)",
     "Intended Audience :: Developers",
