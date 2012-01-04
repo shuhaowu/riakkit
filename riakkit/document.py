shuhaowu/riakkit

commit 965f315ba11a02d3795bef43d664278ca5b4bce3

Fixed more bugs in reference handling
Saving objects with references will always append to the col_name
before. Now it doesn't. It checks if it is already in the list of
references.

riakkit/document.py
@@ -22,7 +22,7 @@ from riakkit.types import BaseProperty, LinkedDocuments, ReferenceBaseProperty, 
 
 from riakkit.exceptions import *
 from riakkit.utils import *
-from copy import copy
+from copy import copy, deepcopy
 
 _document_classes = {}
 
@@ -165,12 +165,12 @@ class Document(object):
     else:
       raise RiakkitError("%s is not a proper key!" % key)
 
-    self._saved = saved
     self._obj = self.bucket.get(self.key) if saved else None
     self._links = {}
     self._data = {}
 
     self.mergeData(kwargs)
+    self._saved = saved
 
     self.__class__.instances[self.key] = self
 
@@ -248,7 +248,7 @@ class Document(object):
     obj = cls(riak_obj.get_key(), saved=True)
     cls.instances[obj.key] = obj
 
-    data = cls._cleanupDataFromDatabase(riak_obj.get_data())
+    data = cls._cleanupDataFromDatabase(deepcopy(riak_obj.get_data()))
     obj._data = data
     links = riak_obj.get_links()
     obj._links = obj.updateLinks(links)
@@ -379,6 +379,9 @@ class Document(object):
     The current implementation checks if the object exists in the database and
     if it has been modified or not.
 
+    This method is bugged when the reference to the object is called and they
+    are modified. Example. list.append will not make saved to be False.
+
     Returns:
       True if saved, False if not.
     """
@@ -434,10 +437,12 @@ class Document(object):
           else:
             docs = self._data[name]
           for doc in docs:
-            current_list = doc._data.get(col_name, [])
-            current_list.append(self)
-            doc._data[col_name] = current_list
-            other_docs_to_be_saved.append(doc)
+            key_list = doc._obj.get_data().get(col_name, [])
+            if self.key not in key_list:
+              current_list = doc._data.get(col_name, [])
+              current_list.append(self)
+              doc._data[col_name] = current_list
+              other_docs_to_be_saved.append(doc)
 
       data_to_be_saved[name] = self._meta["_references"][name].convertToDb(self._data[name])
 
@@ -463,9 +468,10 @@ class Document(object):
         else:
           if col_name:
             current_list = doc._links.get(col_name, [])
-            current_list.append(self)
-            doc._links[col_name] = current_list
-            other_docs_to_be_saved.append(doc)
+            if self.key not in [doc.key for doc in current_list]:
+              current_list.append(self)
+              doc._links[col_name] = current_list
+              other_docs_to_be_saved.append(doc)
 
           if doc._obj is None:
             raise RiakkitError("Add link failure as %s does not exist in the database." % str(doc))
@@ -495,7 +501,7 @@ class Document(object):
     """
     if self._obj:
       self._obj.reload(r=r, vtag=vtag)
-      data = self._cleanupDataFromDatabase(self._obj.get_data())
+      data = self._cleanupDataFromDatabase(deepcopy(self._obj.get_data()))
       self._data = data
       links = self._obj.get_links()
       self._links = self.updateLinks(links)

docs/riakkit.document.html
@@ -152,6 +152,9 @@ Args:<br>
 The&nbsp;current&nbsp;implementation&nbsp;checks&nbsp;if&nbsp;the&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;exists&nbsp;in&nbsp;the&nbsp;database&nbsp;and<br>
 if&nbsp;it&nbsp;has&nbsp;been&nbsp;modified&nbsp;or&nbsp;not.<br>
 &nbsp;<br>
+This&nbsp;method&nbsp;is&nbsp;bugged&nbsp;when&nbsp;the&nbsp;reference&nbsp;to&nbsp;the&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;is&nbsp;called&nbsp;and&nbsp;they<br>
+are&nbsp;modified.&nbsp;Example.&nbsp;list.append&nbsp;will&nbsp;not&nbsp;make&nbsp;saved&nbsp;to&nbsp;be&nbsp;False.<br>
+&nbsp;<br>
 Returns:<br>
 &nbsp;&nbsp;True&nbsp;if&nbsp;saved,&nbsp;False&nbsp;if&nbsp;not.</tt></dd></dl>
 
@@ -175,6 +178,15 @@ Returns:<br>
 
 <hr>
 Class methods defined here:<br>
+<dl><dt><a name="Document-exists"><strong>exists</strong></a>(cls, key, r<font color="#909090">=None</font>)<font color="#909090"><font face="helvetica, arial"> from <a href="riakkit.document.html#DocumentMetaclass">DocumentMetaclass</a></font></font></dt><dd><tt>Check&nbsp;if&nbsp;a&nbsp;key&nbsp;exists.<br>
+&nbsp;<br>
+Args:<br>
+&nbsp;&nbsp;key:&nbsp;The&nbsp;key&nbsp;to&nbsp;check&nbsp;if&nbsp;exists&nbsp;or&nbsp;not.<br>
+&nbsp;&nbsp;r:&nbsp;The&nbsp;R&nbsp;value<br>
+&nbsp;<br>
+Returns:<br>
+&nbsp;&nbsp;True&nbsp;if&nbsp;the&nbsp;key&nbsp;exists,&nbsp;false&nbsp;otherwise.</tt></dd></dl>
+
 <dl><dt><a name="Document-getWithKey"><strong>getWithKey</strong></a>(cls, key, r<font color="#909090">=None</font>)<font color="#909090"><font face="helvetica, arial"> from <a href="riakkit.document.html#DocumentMetaclass">DocumentMetaclass</a></font></font></dt><dd><tt>Get&nbsp;an&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;with&nbsp;a&nbsp;given&nbsp;key.<br>
 &nbsp;<br>
 Args:<br>

docs/riakkit.types.html
@@ -110,6 +110,11 @@ Args:<br>
 <dl><dt><a name="BaseProperty-convertFromDb"><strong>convertFromDb</strong></a>(self, value)</dt><dd><tt>Converts&nbsp;the&nbsp;value&nbsp;from&nbsp;the&nbsp;database&nbsp;back&nbsp;to&nbsp;an&nbsp;app&nbsp;friendly&nbsp;value&nbsp;(a<br>
 value&nbsp;that&nbsp;<a href="#BaseProperty-standardize">standardize</a>()&nbsp;will&nbsp;consider&nbsp;as&nbsp;valid).<br>
 &nbsp;<br>
+Note&nbsp;that&nbsp;the&nbsp;<a href="#BaseProperty">BaseProperty</a>'s&nbsp;convertFromDb&nbsp;will&nbsp;change&nbsp;the&nbsp;value&nbsp;from&nbsp;None<br>
+(if&nbsp;that's&nbsp;how&nbsp;it&nbsp;was&nbsp;in&nbsp;the&nbsp;database)&nbsp;to&nbsp;the&nbsp;default&nbsp;value,&nbsp;if&nbsp;the&nbsp;default<br>
+value&nbsp;is&nbsp;not&nbsp;None.&nbsp;This&nbsp;is&nbsp;done&nbsp;so&nbsp;that&nbsp;any&nbsp;attributes&nbsp;that's&nbsp;added&nbsp;after<br>
+the&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;is&nbsp;saved&nbsp;that&nbsp;has&nbsp;a&nbsp;default&nbsp;value&nbsp;wouldn't&nbsp;be&nbsp;None.<br>
+&nbsp;<br>
 Must&nbsp;be&nbsp;able&nbsp;to&nbsp;handle&nbsp;None.<br>
 &nbsp;<br>
 Args:<br>
@@ -211,6 +216,11 @@ Args:<br>
 
 <dl><dt><a name="BooleanProperty-convertFromDb"><strong>convertFromDb</strong></a>(self, value)</dt><dd><tt>Converts&nbsp;the&nbsp;value&nbsp;from&nbsp;the&nbsp;database&nbsp;back&nbsp;to&nbsp;an&nbsp;app&nbsp;friendly&nbsp;value&nbsp;(a<br>
 value&nbsp;that&nbsp;<a href="#BooleanProperty-standardize">standardize</a>()&nbsp;will&nbsp;consider&nbsp;as&nbsp;valid).<br>
+&nbsp;<br>
+Note&nbsp;that&nbsp;the&nbsp;<a href="#BaseProperty">BaseProperty</a>'s&nbsp;convertFromDb&nbsp;will&nbsp;change&nbsp;the&nbsp;value&nbsp;from&nbsp;None<br>
+(if&nbsp;that's&nbsp;how&nbsp;it&nbsp;was&nbsp;in&nbsp;the&nbsp;database)&nbsp;to&nbsp;the&nbsp;default&nbsp;value,&nbsp;if&nbsp;the&nbsp;default<br>
+value&nbsp;is&nbsp;not&nbsp;None.&nbsp;This&nbsp;is&nbsp;done&nbsp;so&nbsp;that&nbsp;any&nbsp;attributes&nbsp;that's&nbsp;added&nbsp;after<br>
+the&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;is&nbsp;saved&nbsp;that&nbsp;has&nbsp;a&nbsp;default&nbsp;value&nbsp;wouldn't&nbsp;be&nbsp;None.<br>
 &nbsp;<br>
 Must&nbsp;be&nbsp;able&nbsp;to&nbsp;handle&nbsp;None.<br>
 &nbsp;<br>
@@ -423,6 +433,11 @@ Args:<br>
 
 <dl><dt><a name="DynamicProperty-convertFromDb"><strong>convertFromDb</strong></a>(self, value)</dt><dd><tt>Converts&nbsp;the&nbsp;value&nbsp;from&nbsp;the&nbsp;database&nbsp;back&nbsp;to&nbsp;an&nbsp;app&nbsp;friendly&nbsp;value&nbsp;(a<br>
 value&nbsp;that&nbsp;<a href="#DynamicProperty-standardize">standardize</a>()&nbsp;will&nbsp;consider&nbsp;as&nbsp;valid).<br>
+&nbsp;<br>
+Note&nbsp;that&nbsp;the&nbsp;<a href="#BaseProperty">BaseProperty</a>'s&nbsp;convertFromDb&nbsp;will&nbsp;change&nbsp;the&nbsp;value&nbsp;from&nbsp;None<br>
+(if&nbsp;that's&nbsp;how&nbsp;it&nbsp;was&nbsp;in&nbsp;the&nbsp;database)&nbsp;to&nbsp;the&nbsp;default&nbsp;value,&nbsp;if&nbsp;the&nbsp;default<br>
+value&nbsp;is&nbsp;not&nbsp;None.&nbsp;This&nbsp;is&nbsp;done&nbsp;so&nbsp;that&nbsp;any&nbsp;attributes&nbsp;that's&nbsp;added&nbsp;after<br>
+the&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;is&nbsp;saved&nbsp;that&nbsp;has&nbsp;a&nbsp;default&nbsp;value&nbsp;wouldn't&nbsp;be&nbsp;None.<br>
 &nbsp;<br>
 Must&nbsp;be&nbsp;able&nbsp;to&nbsp;handle&nbsp;None.<br>
 &nbsp;<br>
@@ -691,6 +706,11 @@ Args:<br>
 <dl><dt><a name="FloatProperty-convertFromDb"><strong>convertFromDb</strong></a>(self, value)</dt><dd><tt>Converts&nbsp;the&nbsp;value&nbsp;from&nbsp;the&nbsp;database&nbsp;back&nbsp;to&nbsp;an&nbsp;app&nbsp;friendly&nbsp;value&nbsp;(a<br>
 value&nbsp;that&nbsp;<a href="#FloatProperty-standardize">standardize</a>()&nbsp;will&nbsp;consider&nbsp;as&nbsp;valid).<br>
 &nbsp;<br>
+Note&nbsp;that&nbsp;the&nbsp;<a href="#BaseProperty">BaseProperty</a>'s&nbsp;convertFromDb&nbsp;will&nbsp;change&nbsp;the&nbsp;value&nbsp;from&nbsp;None<br>
+(if&nbsp;that's&nbsp;how&nbsp;it&nbsp;was&nbsp;in&nbsp;the&nbsp;database)&nbsp;to&nbsp;the&nbsp;default&nbsp;value,&nbsp;if&nbsp;the&nbsp;default<br>
+value&nbsp;is&nbsp;not&nbsp;None.&nbsp;This&nbsp;is&nbsp;done&nbsp;so&nbsp;that&nbsp;any&nbsp;attributes&nbsp;that's&nbsp;added&nbsp;after<br>
+the&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;is&nbsp;saved&nbsp;that&nbsp;has&nbsp;a&nbsp;default&nbsp;value&nbsp;wouldn't&nbsp;be&nbsp;None.<br>
+&nbsp;<br>
 Must&nbsp;be&nbsp;able&nbsp;to&nbsp;handle&nbsp;None.<br>
 &nbsp;<br>
 Args:<br>
@@ -765,6 +785,11 @@ Args:<br>
 
 <dl><dt><a name="IntegerProperty-convertFromDb"><strong>convertFromDb</strong></a>(self, value)</dt><dd><tt>Converts&nbsp;the&nbsp;value&nbsp;from&nbsp;the&nbsp;database&nbsp;back&nbsp;to&nbsp;an&nbsp;app&nbsp;friendly&nbsp;value&nbsp;(a<br>
 value&nbsp;that&nbsp;<a href="#IntegerProperty-standardize">standardize</a>()&nbsp;will&nbsp;consider&nbsp;as&nbsp;valid).<br>
+&nbsp;<br>
+Note&nbsp;that&nbsp;the&nbsp;<a href="#BaseProperty">BaseProperty</a>'s&nbsp;convertFromDb&nbsp;will&nbsp;change&nbsp;the&nbsp;value&nbsp;from&nbsp;None<br>
+(if&nbsp;that's&nbsp;how&nbsp;it&nbsp;was&nbsp;in&nbsp;the&nbsp;database)&nbsp;to&nbsp;the&nbsp;default&nbsp;value,&nbsp;if&nbsp;the&nbsp;default<br>
+value&nbsp;is&nbsp;not&nbsp;None.&nbsp;This&nbsp;is&nbsp;done&nbsp;so&nbsp;that&nbsp;any&nbsp;attributes&nbsp;that's&nbsp;added&nbsp;after<br>
+the&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;is&nbsp;saved&nbsp;that&nbsp;has&nbsp;a&nbsp;default&nbsp;value&nbsp;wouldn't&nbsp;be&nbsp;None.<br>
 &nbsp;<br>
 Must&nbsp;be&nbsp;able&nbsp;to&nbsp;handle&nbsp;None.<br>
 &nbsp;<br>
@@ -856,6 +881,11 @@ Methods inherited from <a href="riakkit.types.html#BaseProperty">BaseProperty</a
 <dl><dt><a name="LinkedDocuments-convertFromDb"><strong>convertFromDb</strong></a>(self, value)</dt><dd><tt>Converts&nbsp;the&nbsp;value&nbsp;from&nbsp;the&nbsp;database&nbsp;back&nbsp;to&nbsp;an&nbsp;app&nbsp;friendly&nbsp;value&nbsp;(a<br>
 value&nbsp;that&nbsp;<a href="#LinkedDocuments-standardize">standardize</a>()&nbsp;will&nbsp;consider&nbsp;as&nbsp;valid).<br>
 &nbsp;<br>
+Note&nbsp;that&nbsp;the&nbsp;<a href="#BaseProperty">BaseProperty</a>'s&nbsp;convertFromDb&nbsp;will&nbsp;change&nbsp;the&nbsp;value&nbsp;from&nbsp;None<br>
+(if&nbsp;that's&nbsp;how&nbsp;it&nbsp;was&nbsp;in&nbsp;the&nbsp;database)&nbsp;to&nbsp;the&nbsp;default&nbsp;value,&nbsp;if&nbsp;the&nbsp;default<br>
+value&nbsp;is&nbsp;not&nbsp;None.&nbsp;This&nbsp;is&nbsp;done&nbsp;so&nbsp;that&nbsp;any&nbsp;attributes&nbsp;that's&nbsp;added&nbsp;after<br>
+the&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;is&nbsp;saved&nbsp;that&nbsp;has&nbsp;a&nbsp;default&nbsp;value&nbsp;wouldn't&nbsp;be&nbsp;None.<br>
+&nbsp;<br>
 Must&nbsp;be&nbsp;able&nbsp;to&nbsp;handle&nbsp;None.<br>
 &nbsp;<br>
 Args:<br>
@@ -936,6 +966,11 @@ Args:<br>
 
 <dl><dt><a name="ListProperty-convertFromDb"><strong>convertFromDb</strong></a>(self, value)</dt><dd><tt>Converts&nbsp;the&nbsp;value&nbsp;from&nbsp;the&nbsp;database&nbsp;back&nbsp;to&nbsp;an&nbsp;app&nbsp;friendly&nbsp;value&nbsp;(a<br>
 value&nbsp;that&nbsp;<a href="#ListProperty-standardize">standardize</a>()&nbsp;will&nbsp;consider&nbsp;as&nbsp;valid).<br>
+&nbsp;<br>
+Note&nbsp;that&nbsp;the&nbsp;<a href="#BaseProperty">BaseProperty</a>'s&nbsp;convertFromDb&nbsp;will&nbsp;change&nbsp;the&nbsp;value&nbsp;from&nbsp;None<br>
+(if&nbsp;that's&nbsp;how&nbsp;it&nbsp;was&nbsp;in&nbsp;the&nbsp;database)&nbsp;to&nbsp;the&nbsp;default&nbsp;value,&nbsp;if&nbsp;the&nbsp;default<br>
+value&nbsp;is&nbsp;not&nbsp;None.&nbsp;This&nbsp;is&nbsp;done&nbsp;so&nbsp;that&nbsp;any&nbsp;attributes&nbsp;that's&nbsp;added&nbsp;after<br>
+the&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;is&nbsp;saved&nbsp;that&nbsp;has&nbsp;a&nbsp;default&nbsp;value&nbsp;wouldn't&nbsp;be&nbsp;None.<br>
 &nbsp;<br>
 Must&nbsp;be&nbsp;able&nbsp;to&nbsp;handle&nbsp;None.<br>
 &nbsp;<br>
@@ -1074,6 +1109,11 @@ Args:<br>
 Methods inherited from <a href="riakkit.types.html#BaseProperty">BaseProperty</a>:<br>
 <dl><dt><a name="ReferenceBaseProperty-convertFromDb"><strong>convertFromDb</strong></a>(self, value)</dt><dd><tt>Converts&nbsp;the&nbsp;value&nbsp;from&nbsp;the&nbsp;database&nbsp;back&nbsp;to&nbsp;an&nbsp;app&nbsp;friendly&nbsp;value&nbsp;(a<br>
 value&nbsp;that&nbsp;<a href="#ReferenceBaseProperty-standardize">standardize</a>()&nbsp;will&nbsp;consider&nbsp;as&nbsp;valid).<br>
+&nbsp;<br>
+Note&nbsp;that&nbsp;the&nbsp;<a href="#BaseProperty">BaseProperty</a>'s&nbsp;convertFromDb&nbsp;will&nbsp;change&nbsp;the&nbsp;value&nbsp;from&nbsp;None<br>
+(if&nbsp;that's&nbsp;how&nbsp;it&nbsp;was&nbsp;in&nbsp;the&nbsp;database)&nbsp;to&nbsp;the&nbsp;default&nbsp;value,&nbsp;if&nbsp;the&nbsp;default<br>
+value&nbsp;is&nbsp;not&nbsp;None.&nbsp;This&nbsp;is&nbsp;done&nbsp;so&nbsp;that&nbsp;any&nbsp;attributes&nbsp;that's&nbsp;added&nbsp;after<br>
+the&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;is&nbsp;saved&nbsp;that&nbsp;has&nbsp;a&nbsp;default&nbsp;value&nbsp;wouldn't&nbsp;be&nbsp;None.<br>
 &nbsp;<br>
 Must&nbsp;be&nbsp;able&nbsp;to&nbsp;handle&nbsp;None.<br>
 &nbsp;<br>
@@ -1220,6 +1260,11 @@ Args:<br>
 
 <dl><dt><a name="StringProperty-convertFromDb"><strong>convertFromDb</strong></a>(self, value)</dt><dd><tt>Converts&nbsp;the&nbsp;value&nbsp;from&nbsp;the&nbsp;database&nbsp;back&nbsp;to&nbsp;an&nbsp;app&nbsp;friendly&nbsp;value&nbsp;(a<br>
 value&nbsp;that&nbsp;<a href="#StringProperty-standardize">standardize</a>()&nbsp;will&nbsp;consider&nbsp;as&nbsp;valid).<br>
+&nbsp;<br>
+Note&nbsp;that&nbsp;the&nbsp;<a href="#BaseProperty">BaseProperty</a>'s&nbsp;convertFromDb&nbsp;will&nbsp;change&nbsp;the&nbsp;value&nbsp;from&nbsp;None<br>
+(if&nbsp;that's&nbsp;how&nbsp;it&nbsp;was&nbsp;in&nbsp;the&nbsp;database)&nbsp;to&nbsp;the&nbsp;default&nbsp;value,&nbsp;if&nbsp;the&nbsp;default<br>
+value&nbsp;is&nbsp;not&nbsp;None.&nbsp;This&nbsp;is&nbsp;done&nbsp;so&nbsp;that&nbsp;any&nbsp;attributes&nbsp;that's&nbsp;added&nbsp;after<br>
+the&nbsp;<a href="__builtin__.html#object">object</a>&nbsp;is&nbsp;saved&nbsp;that&nbsp;has&nbsp;a&nbsp;default&nbsp;value&nbsp;wouldn't&nbsp;be&nbsp;None.<br>
 &nbsp;<br>
 Must&nbsp;be&nbsp;able&nbsp;to&nbsp;handle&nbsp;None.<br>
 &nbsp;<br>

riakkit/types.py
@@ -85,11 +85,19 @@ class BaseProperty(object):
     """Converts the value from the database back to an app friendly value (a
     value that standardize() will consider as valid).
 
+    Note that the BaseProperty's convertFromDb will change the value from None
+    (if that's how it was in the database) to the default value, if the default
+    value is not None. This is done so that any attributes that's added after
+    the object is saved that has a default value wouldn't be None.
+
     Must be able to handle None.
 
     Args:
       value: The value to be converted
     """
+    default = self.defaultValue()
+    if value is None and default is not None:
+      value = default
     return self._processValue(value, self.backwardprocessors)
 
   def standardize(self, value):

commit 9599cada3ec1eaf2c73fa26d967bd22c5f4e679e

Fixed a bug introduced by 965f315ba11a02d3795be
The previous commit, 965f315ba11a02d3795bef43d664278ca5b4bce3,
introduced 2 bugs that caused a failure for linking multiple documents,
as well as another bug in reference handling.

Apparently running the doctests before commit is very important.

riakkit/document.py
@@ -437,9 +437,9 @@ class Document(object):
           else:
             docs = self._data[name]
           for doc in docs:
-            key_list = doc._obj.get_data().get(col_name, [])
+            current_list = doc._data.get(col_name, [])
+            key_list = [d.key for d in current_list]
             if self.key not in key_list:
-              current_list = doc._data.get(col_name, [])
               current_list.append(self)
               doc._data[col_name] = current_list
               other_docs_to_be_saved.append(doc)
@@ -466,15 +466,17 @@ class Document(object):
         if not isinstance(doc, Document):
           raise AttributeError("%s is not a Document instance!" % item)
         else:
+          if doc._obj is None:
+            raise RiakkitError("Add link failure as %s does not exist in the database." % str(doc))
+
           if col_name:
             current_list = doc._links.get(col_name, [])
-            if self.key not in [doc.key for doc in current_list]:
+            if self.key not in [d.key for d in current_list]:
               current_list.append(self)
               doc._links[col_name] = current_list
               other_docs_to_be_saved.append(doc)
 
-          if doc._obj is None:
-            raise RiakkitError("Add link failure as %s does not exist in the database." % str(doc))
+
           self._obj.add_link(doc._obj, name) # TODO: doc._obj=None
 
     self._obj.store(w=w, dw=dw)