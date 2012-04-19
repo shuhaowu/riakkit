c657b2283b3161fd434c092794943d571ce3fed0
Shuhao <admin@thekks.net>
2012-04-19 17:24:20 -0400

    Rewrite starts.

--- examples/blog/settings.py
+++
@@ -1,5 +0,0 @@
-# This file is not part of the license and is public domain.
-DEBUG = True
-DEPLOY_PORT = 8000
-SALT = "x+m17|d)g9K4|0N6Bxk8" # Change it if you want to use it.
-SECRET_KEY = "u7l-2.<04.57_1<m8NrG"
